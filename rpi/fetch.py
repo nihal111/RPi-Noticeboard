e5c4a2c2ecc3037d6bf160cce63fadb92dbb378a
Nihal Singh <nihal.111@gmail.com>
2018-01-31 19:47:29 +0530

    Modified fetch script

--- rpi/fetch.py
+++ rpi/fetch.py
@@ -20,7 +20,9 @@ url = urljoin(BASEURL, 'list/')
 response = urllib.urlopen(url)
 data = json.loads(response.read())
 
-# print data['galleries']['0']
+online_list = []
+files_to_delete = []
+existing_files = []
 
 for g_index in data['galleries']:
     gallery = data['galleries'][g_index]
@@ -33,4 +35,40 @@ for g_index in data['galleries']:
         img_url = photo['url']
         url = urljoin(BASEURL, img_url)
         name = photo['name']
-        urllib.urlretrieve(url, os.path.join(BASE_DIR, title, name))
+        online_list.append((url, os.path.join(BASE_DIR, title, name)))
+        # urllib.urlretrieve(url, os.path.join(TEMP_DIR, title, name))
+
+print
+print "Online List- " + str(len(online_list))
+print online_list
+
+for (dirpath, dirnames, filenames) in os.walk(BASE_DIR):
+    for file in filenames:
+        file_path = os.path.join(dirpath, file)
+        existing_files.append(file_path)
+        found = False
+        for online_file in online_list:
+            if online_file[1] == file_path:
+                found = True
+                break
+        if found is False:
+            files_to_delete.append(file_path)
+print
+print "Existing Files- " + str(len(existing_files))
+print existing_files
+
+print
+print "Files to delete- " + str(len(files_to_delete))
+print files_to_delete
+
+
+for file in online_list:
+    if file[1] not in existing_files:
+        urllib.urlretrieve(file[0], file[1])
+
+for file in files_to_delete:
+    os.remove(file)
+
+
+# shutil.rmtree(BASE_DIR)
+# os.rename(TEMP_DIR, BASE_DIR)
