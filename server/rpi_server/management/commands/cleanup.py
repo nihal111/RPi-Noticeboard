3079f6c1783e77a9b455a40d1183334470fe1e88
Nihal Singh <nihal.111@gmail.com>
2018-02-08 19:16:59 +0530

    Added date log to cleanup

--- server/rpi_server/management/commands/cleanup.py
+++ server/rpi_server/management/commands/cleanup.py
@@ -7,6 +7,7 @@ class Command(BaseCommand):
     help = 'Deletes photos which have expired.'
 
     def handle(self, *args, **options):
+        print("Running on " + str(datetime.date.today()))
         photos = Photos.objects.all().filter(
             expiry_date__lt=datetime.date.today())
         for photo in photos:
@@ -15,3 +16,4 @@ class Command(BaseCommand):
                 .format(photo, photo.expiry_date)))
             Photos.objects.filter(id=photo.id).delete()
         self.stdout.write(self.style.SUCCESS('Deleted all old photos'))
+        print
